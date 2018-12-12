3022ed435215409fa1787ab2b85cfa1e3f28f659
zhou <18081047182@163.com>
2018-12-12 15:00:50 +0800

    no message

--- app/src/main/java/com/system/tianma/MainActivity.java
+++ app/src/main/java/com/system/tianma/MainActivity.java
@@ -3,12 +3,14 @@ package com.system.tianma;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.system.myproject.BaseConfig;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
 }
